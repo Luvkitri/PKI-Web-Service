3efff77fe10bf558e6350d46db1546e99be89d74
Luvkitri <janjedrzejewski@gmail.com>
2020-09-10 23:46:04 +0200

    POST request handler for displaying table content

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const pool = require('./db.js');
 const app = express();
 
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Hello World!!!');
+});
+
 // Routes
 
 // Get all tables
@@ -19,6 +23,23 @@ app.get('/tables', async (req, res) => {
         console.error(err.message);
     }
 });
+
+// Post table content
+app.post('/table', async (req, res) => {
+    try {
+        const table_name = req.body;
+        let table = await pool.query(
+            `SELECT * FROM ${table_name}`
+        );
+
+        res.json(table.rows);
+    } catch (err) {
+        console.error(err.message);
+    }
+});
+
+// Post query
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
