0a901602a3354ee50b3ae7d1a94d41ec3f713f10
Luvkitri <janjedrzejewski@gmail.com>
2020-09-10 23:45:20 +0200

    GET request handler for getting all tables.

--- app.js
+++ app.js
@@ -4,7 +4,21 @@ const app = express();
 
 app.get('/', (req, res) => {
     res.send('Hello World!!!');
-})
+// Routes
+
+// Get all tables
+app.get('/tables', async (req, res) => {
+    try {
+        let tables = await pool.query(
+            `SELECT table_name FROM information_schema.tables 
+            WHERE table_schema='public' AND table_type='BASE TABLE';`
+        );
+        
+        res.json(tables.rows);
+    } catch (err) {
+        console.error(err.message);
+    }
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
